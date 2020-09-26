363bbbba7a2cb6c897d5212d340cf55c258e08d0
dikshaupmanyu <diksha.novasoftcorps@gmail.com>
2020-09-26 03:46:18 -0700

    changes

---
+++ build/precache-manifest.8b356d243d4322d3bf895d20bf49b04c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792b7e80c9a59b4e7808afefbc7c2da0",
+    "url": "/dikshaupmanyu/myapp/index.html"
+  },
+  {
+    "revision": "91664e59d597c792d88a",
+    "url": "/dikshaupmanyu/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/dikshaupmanyu/myapp/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dikshaupmanyu/myapp/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91664e59d597c792d88a",
+    "url": "/dikshaupmanyu/myapp/static/js/main.bd4f2bd6.chunk.js"
+  },
+  {
+    "revision": "e1c3b2d81b7c0244719c",
+    "url": "/dikshaupmanyu/myapp/static/js/runtime-main.b09db0c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dikshaupmanyu/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
